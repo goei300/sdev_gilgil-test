3c450d12ed4ee4fc9f312bc4de5f62b73dd11c12
goei300 <goei4559@gmail.com>
2024-03-08 10:53:11 +0900

    [feat] user's input func

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <iostream>
+#include <cstring>
 #include "sum.h"
 
-int main() {
-	int s = sum(10);
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+    int n=0;
+    int d=1;
+    int nsize = strlen(argv[1]);
+
+    for(int i=nsize-1;i>=0;i--){
+        n += (argv[1][i]-'0')*d;
+        d*=10;
+    }
+	int s = sum(n);
+
 	printf("result=%d\n", s);
 }
